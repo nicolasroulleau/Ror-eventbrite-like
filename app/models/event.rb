7e31644cd585d0ac92d775e2be7ca64c283fc1e7
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-02-09 09:48:28 +0100

    test out

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
 
-belongs_to :creator, :foreign_key => "creator_id", :class_name => "User"
+belongs_to :creator, :class_name => "User"
 has_and_belongs_to_many :attendees, :class_name => "User"
 
 end
